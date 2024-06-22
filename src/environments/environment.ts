6b3aa04d275086a285d3c09d94c9019a53e85a8d
Henrique Higa <codehiga@gmail.com>
2024-06-21 23:08:32 -0300

    feat: ajkustes visuais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
     stripePublishableKey: 'pk_live_51PNko5Ju8JtNTZAxOSoobG6IV053aUXD3CYOu7bd5oNqBeiswIE02rOYDCARGJkmWaOWmot4VpGuURtAslQuiSzM00Nba4bC1I',
-
-
 };
