c5647ebefee4cb23cdf30c948ca53e5dc4608561
codeByEdu <epereira607@gmail.com>
2024-06-26 23:34:16 -0300

    ajuste da tela de cerimonia

--- src/app/components/lista-presentes/lista-presentes.component.ts
+++ src/app/components/lista-presentes/lista-presentes.component.ts
@@ -22,7 +22,8 @@ export class ListaPresentesComponent implements OnInit {
             this.presentes = res.data;
             console.log(this.presentes);
             this.presentes.forEach((presente: any) => {
-                presente.metadata.moeda = presente.metadata.moeda.replaceAll("'", "");
+                presente.metadata.moeda = presente.metadata.moeda.replace("'", "");
+                presente.metadata.moeda = presente.metadata.moeda.replace("'", "");
             })
         })
     }
